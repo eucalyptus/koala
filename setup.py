eacbac7adca45d415716582c274f53d473dad260
dkavanagh <dkavanagh@gmail.com>
2015-07-29 08:16:03 -0400

    less than 2.0, duh

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ requires = [
     'pyramid_beaker >= 0.8',
     'pyramid_chameleon >= 0.1',
     'pyramid_layout <= 0.8',
-    'python-dateutil >= 1.4.1, <= 2.0',  # Don't use 2.x series unless on Python 3
+    'python-dateutil >= 1.4.1, < 2.0',  # Don't use 2.x series unless on Python 3
     'python-magic >= 0.4.6',
     'simplejson >= 2.0.9',
     'WTForms >= 1.0.2',
